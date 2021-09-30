6fd7a007e198380b775bf0630f82a639df02366a
Wasi Haider <iwasihaaider@gmail.com>
2021-09-30 18:11:40 +0500

    Thursday Finishing Weather App

---
+++ WheatherApp/StoryBoard.swift
@@ -0,0 +1,34 @@
+//
+//  StoryBoard.swift
+//  Travel
+//
+//  Created by Dev ILI on 06/08/2021.
+//
+
+import Foundation
+
+
+
+enum StoryboardReference: String {
+    case Main
+    case Profile
+}
+
+enum ViewControllerReference: String {
+    case GuideTwoViewController
+    case guidethreeViewController
+    case Login1ViewController
+    case login2
+    case SignuppageViewController
+    case enterphonenoViewController
+    case recoverpassViewController
+    case ProfileViewController
+    case ProfileTwo
+    case ProfileThree
+    case ProfileFour
+    case FirstContainer
+    case SettingViewController
+    case TermsAndServiceViewController
+    case PrivacyPolicy
+    case FAQpage
+ }
